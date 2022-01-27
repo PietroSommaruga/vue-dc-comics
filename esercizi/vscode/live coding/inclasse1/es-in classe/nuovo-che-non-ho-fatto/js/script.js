a8b8a45c11c080f794e40f6d8c12c42c1accdcc5
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-27 18:37:30 +0100

    esercizio

--- esercizi/vscode/live coding/inclasse1/es-in classe/nuovo-che-non-ho-fatto/js/script.js
+++
@@ -1,8 +0,0 @@
-const iscritti = ["Gianni", "paolo", "marco", "antonio", "paolo", "teo", "fede", "como", "lallo",]
-
-
-// -----------------------------------
-
-for (let i = 0;i < iscritti.length; i++){
-    console.log(iscritti[i])
-}
